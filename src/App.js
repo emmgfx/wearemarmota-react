d738614c1ef2bd5c0a682bc28e7dc794519084c0
Josep Viciana <emmgfx@gmail.com>
2019-03-05 22:04:02 +0100

    Tab corrections

--- src/App.js
+++ src/App.js
@@ -14,24 +14,24 @@ import Legal from './pages/Legal';
 import './scss/style.scss';
 
 class App extends Component {
-  render() {
-    return (
-        <Router>
-            <React.Fragment>
-                
-                <Header />
-                <Route exact path="/" component={Home} />
-                <Route exact path="/ui-ux" component={UiUx} />
-                <Route exact path="/android" component={Android} />
-                <Route exact path="/development" component={Development} />
-                <Route exact path="/design" component={Design} />
-                <Route exact path="/legal" component={Legal} />
-                <Footer />
-                
-            </React.Fragment>
-        </Router>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <React.Fragment>
+                    
+                    <Header />
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/ui-ux" component={UiUx} />
+                    <Route exact path="/android" component={Android} />
+                    <Route exact path="/development" component={Development} />
+                    <Route exact path="/design" component={Design} />
+                    <Route exact path="/legal" component={Legal} />
+                    <Footer />
+                    
+                </React.Fragment>
+            </Router>
+        );
+    }
 }
 
 export default App;
